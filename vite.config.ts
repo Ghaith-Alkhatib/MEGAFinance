30902243384f9b8fa1a5f8930d5cb70c5899f863
Ghaith-Alkhatib <khateeb211@googlemail.com>
2025-03-20 14:47:51 +0300

    Update package.json and vite.config.ts for project optimization

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/MEGAFinance/', // اسم المستودع كما يظهر على GitHub
+  base: '/MEGAFinanceJo/', 
 });
